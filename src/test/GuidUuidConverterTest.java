71233716e108e625edc8656f8097669c7c893d54
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2020-10-31 12:01:23 +0100

    add converter and basic tests

---
+++ src/test/GuidUuidConverterTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import main.com.uniejewski.GuidUuidConverter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GuidUuidConverterTest {
+
+    @DataProvider
+    private Object[][] guidsAndUuids() {
+        return new Object[][]{
+                {"23F80AE87D7D0D4B8FDFA3B20E02F878", "23f80ae8-7d7d-0d4b-8fdf-a3b20e02f878"},
+                {"E43179F0E4D3F145A32EC2F711F4C7BC", "e43179f0-e4d3-f145-a32e-c2f711f4c7bc"}
+        };
+    }
+
+    @Test(dataProvider = "guidsAndUuids")
+    public void testConvertGuidToUuid(String givenGuid, String expectedUuid) {
+        assertEquals(GuidUuidConverter.convertGuidToUuid(givenGuid), expectedUuid);
+    }
+
+    @Test(dataProvider = "guidsAndUuids")
+    public void testConvertUuidToGuid(String expectedGuid, String givenUuid) {
+        assertEquals(GuidUuidConverter.convertUuidToGuid(givenUuid), expectedGuid);
+    }
+
+}
